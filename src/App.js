d5b228d7d7d9f396cd4e7217431bb7cc6aa07ec0
Nikita Patidar <patidarnikita36@gmail.com>
2023-11-01 13:15:35 +0530

    till update todo

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = ()=>
       addToArray(todo[todo.length-1].id+1,tempTodo,false)
     }
     else
-    {
+    { 
       addToArray(0,tempTodo,false)
     }
    
@@ -99,6 +99,22 @@ const App = ()=>
     setTodo([...todo])
     
   }
+
+  function updateTodo()
+  {
+    console.log("-------Update todo-----")
+    console.log("editingFlage: "+ editingFlage)
+    let tempTodo = todo.filter(element=>
+    {
+      if(element.id == editingFlage)
+      {
+        element.text = document.getElementById("editTodo").value
+
+      }
+      return element
+    })
+    setTodo([...tempTodo])
+  }
   
 
    return <div>
@@ -118,21 +134,22 @@ const App = ()=>
                     <s>{element.text+" "}</s>
                    {/* <button onClick={()=>deleteTodo(element.id)}>Delete</button> */}
                    </div>:
-                    {element.id == editingFlage?
+                   //* incompleted todo 
+                    (element.id == editingFlage?
                     <div>  
-                    {/* incompleted todo */}
+                       {/* editing frontend */}
                   
-                    <input type="checkbox" onClick={()=>checkListener(element.id)} ></input>
-         
-                    {element.text+" "}
-                    <button onClick={()=>deleteTodo(element.id)} >Delete</button>
-                     <button onClick={()=>editTodo(element.id)}>Edite</button>
+                     <input type="checkbox" onClick={()=>checkListener(element.id)} ></input>
+                     <input type="text" value={element.text} placeholder="Update Todo here" id="editTodo"/>
+                   
+                     <button onClick={()=>deleteTodo(element.id)} >Delete</button>
+                     <button onClick={()=>updateTodo()}>Save Todo</button>
           
                     </div>
                     :
-                  
-                     <div>  
-                    {/* incompleted todo */}
+                     //default frontend
+                    <div>  
+                    
                   
                       <input type="checkbox" onClick={()=>checkListener(element.id)} ></input>
          
@@ -141,7 +158,7 @@ const App = ()=>
                       <button onClick={()=>editTodo(element.id)}>Edite</button>
           
                     </div>
-                   }
+                    )
                   }
                   </div>
         })
